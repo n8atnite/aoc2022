592b2f1f95f2187a68abf54b571b4112e000e2b1
Nate Atkinson <atkies@ucla.edu>
2022-12-04 23:02:36 -0800

    day 4

---
+++ days/4/sol1.py
@@ -0,0 +1,22 @@
+def is_contained(a, b):
+    '''
+    check if a is contained within b
+    '''
+    if a[0] >= b[0]:
+        if a[1] <= b[1]:
+            return True
+    return False
+
+with open('input4.txt', 'r') as f:
+    entries = [line.replace('\n', '') for line in f.readlines()]
+
+contained = 0
+for entry in entries:
+    check = False
+    assignments = [[int(i) for i in pair.split('-')] for pair in entry.split(',')]
+    if is_contained(assignments[0], assignments[1]):
+        contained += 1
+    elif is_contained(assignments[1], assignments[0]):
+        contained += 1
+
+print(contained)
---
+++ days/4/sol2.py
@@ -0,0 +1,20 @@
+def is_overlapped(a, b):
+    '''
+    check if a is contained within b
+    '''
+    if a[0] <= b[1]:
+        if a[1] >= b[0]:
+            return True
+    return False
+
+with open('input4.txt', 'r') as f:
+    entries = [line.replace('\n', '') for line in f.readlines()]
+
+overlapped = 0
+for entry in entries:
+    check = False
+    assignments = [[int(i) for i in pair.split('-')] for pair in entry.split(',')]
+    if is_overlapped(assignments[0], assignments[1]):
+        overlapped += 1
+
+print(overlapped)
