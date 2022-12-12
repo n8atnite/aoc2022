02beda004acd8e2f6270561f1ba25f29d907f134
Nate Atkinson <atkies@ucla.edu>
2022-12-11 17:41:35 -0800

    day 11

---
+++ days/11/sol1.py
@@ -0,0 +1,62 @@
+import re
+import operator as ops
+from collections import OrderedDict as odict
+
+OPS = {
+    '+': ops.add,
+    '-': ops.sub,
+    '*': ops.mul,
+    '/': ops.floordiv
+}
+
+class Monkey:
+    def __init__(self, startingItems, funcArgs, testValue, truePartner, falsePartner):
+        self.inventory = startingItems
+        
+        self.operation = lambda x: funcArgs[0](x, (x if funcArgs[1] == 'old' else int(funcArgs[1])))
+        self.test = testValue
+        self.truePartner = truePartner
+        self.falsePartner = falsePartner
+        self.activity = 0
+
+    def inspectItem(self):
+        item = self.operation(self.inventory.pop(0))
+        throwable = item // 3
+        if throwable%self.test == 0:
+            partner = self.truePartner
+        else:
+            partner = self.falsePartner
+
+        self.activity += 1
+        return partner, throwable
+
+with open('input11.txt', 'r') as f:
+    entries = [[word for word in line.split(' ') if word] for line in f.read().splitlines() if line]
+
+# populate fields
+monkeys = odict()
+params = []
+for i, entry in enumerate(entries):
+    index = i%6
+    if index == 0:
+        params.append(int(entry[1][0]))
+    elif index == 1:
+        params.append([int(re.sub("[^0-9]", "", num)) for num in entry[2:]])
+    elif index == 2:
+        params.append((OPS[entry[4]], entry[5]))
+    elif index == 3:
+        params.append(int(entry[3]))
+    elif index == 4:
+        params.append(int(entry[5]))
+    elif index == 5:
+        params.append(int(entry[5]))
+        monkeys[params[0]] = Monkey(*params[1:])
+        params = []
+
+for round in range(20):
+    for monkey in monkeys.values():
+        while monkey.inventory:
+            res = monkey.inspectItem()
+            monkeys[res[0]].inventory.append(res[1])
+
+print(ops.mul(*sorted([m.activity for m in monkeys.values()], reverse=True)[:2]))
